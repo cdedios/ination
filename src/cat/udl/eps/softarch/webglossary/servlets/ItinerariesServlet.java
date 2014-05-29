4f1d453fabcdd7eed15872464318d0dfe23a51d0
Carlos de Dios <cdedios92@gmail.com>
2014-05-29 16:27:45 +0200

    ItinerariesServlet & web.xml modified

--- src/cat/udl/eps/softarch/webglossary/servlets/ItinerariesServlet.java
+++ src/cat/udl/eps/softarch/webglossary/servlets/ItinerariesServlet.java
@@ -1,12 +1,15 @@
 package cat.udl.eps.softarch.webglossary.servlets;
 
 import cat.udl.eps.softarch.webglossary.model.Alert;
+import cat.udl.eps.softarch.webglossary.model.Glossary;
+import cat.udl.eps.softarch.webglossary.model.GlossaryEntry;
 import cat.udl.eps.softarch.webglossary.model.Itinerary;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -15,14 +18,43 @@ import java.util.ArrayList;
 /**
  * Created by albertbergespeiro on 26/05/14.
  */
-public class ItinerariesServlet {
+public class ItinerariesServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         UserService userService = UserServiceFactory.getUserService();
-        User user = userService.getCurrentUser();
+        User currentUser = userService.getCurrentUser();
 
-        ArrayList<Itinerary> alerts = Itinerary.getStoredItineraries(user.getUserId());
+        if (currentUser != null) {
+            ArrayList<Itinerary> alerts = Itinerary.getStoredItineraries(currentUser.getUserId());
+
+            request.setAttribute("alerts", alerts);
+            request.getRequestDispatcher("itineriaries.jsp").forward(request, response);
+
+        } else {
+            response.sendRedirect(userService.createLoginURL(request.getRequestURI()));
+        }
+
 
         /*request.setAttribute("alerts", alerts);
         request.getRequestDispatcher("list.jsp").forward(request, response);*/
+
     }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UserService userService = UserServiceFactory.getUserService();
+        User currentUser = userService.getCurrentUser();
+        if (currentUser != null) {
+            String road = request.getParameter("road");
+            double start =  Double.parseDouble(request.getParameter("start"));
+            double end =  Double.parseDouble(request.getParameter("end"));
+            boolean enabled = Boolean.parseBoolean(request.getParameter("enabled"));
+
+            Itinerary.addItinerary(new Itinerary(currentUser.getEmail(),start,end),enabled);
+
+            response.setContentType("text/plain");
+            response.getWriter().println(currentUser.getNickname()+" added a new entry.");
+        } else {
+            response.sendRedirect(userService.createLoginURL(request.getRequestURI()));
+        }
+    }
+
 }
