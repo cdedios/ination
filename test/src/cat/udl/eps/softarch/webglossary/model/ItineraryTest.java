3d84d659187faa614d42d7a427e8b420b5cb7c47
Carlos de Dios <cdedios92@gmail.com>
2014-05-29 18:39:50 +0200

    Itinerary Test

---
+++ test/src/cat/udl/eps/softarch/webglossary/model/ItineraryTest.java
@@ -0,0 +1,13 @@
+package cat.udl.eps.softarch.webglossary.model;
+
+import org.junit.Test;
+
+/**
+ * Created by carlos on 5/29/14.
+ */
+public class ItineraryTest {
+    @Test
+    public void testAddItinerary() throws Exception {
+
+    }
+}
