77276dbfb6973b7edcf9d70574135ea9831dc3e6
Albert Berges Peiro <albertbergespeiro@aules-102-076.udl.net>
2014-05-29 15:28:54 +0200

    Itinerary started

--- src/cat/udl/eps/softarch/webglossary/model/Itinerary.java
+++ src/cat/udl/eps/softarch/webglossary/model/Itinerary.java
@@ -1,11 +1,10 @@
 package cat.udl.eps.softarch.webglossary.model;
 
+import cat.udl.eps.softarch.webglossary.persistence.EMF;
 import com.google.appengine.api.datastore.Key;
 
-import javax.persistence.Id;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import javax.persistence.*;
+import java.util.ArrayList;
 
 /**
  * Created by Carlos on 14/05/2014.
@@ -58,5 +57,48 @@ public class Itinerary {
 
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
+    }
+
+    public static ArrayList<Itinerary> getStoredItineraries(String id) {
+        ArrayList<Itinerary> itineraries = new ArrayList<Itinerary>();
+        EntityManager em = EMF.get().createEntityManager();
+        try {
+            Query q = em.createQuery("SELECT t FROM Itinerary t "+
+                    "WHERE t.user =" + id);
+            itineraries = new ArrayList<Itinerary>(q.getResultList());
+        } finally {
+            em.close();
+        }
+        return itineraries;
+    }
+
+    public static void addItinerary(Itinerary itinerary) {
+        EntityManager em = EMF.get().createEntityManager();
+        EntityTransaction txn = em.getTransaction();
+        try {
+            txn.begin();
+            em.persist(itinerary);
+            txn.commit(); }
+        catch (Exception e) {
+            //log.warning(e.getMessage());
+        }
+        finally {
+            if (txn.isActive()) txn.rollback();
+            em.close(); }
+    }
+
+    public static void removeItinerary(Itinerary itinerary) {
+        EntityManager em = EMF.get().createEntityManager();
+        EntityTransaction txn = em.getTransaction();
+        try {
+            txn.begin();
+            em.remove(itinerary);
+            txn.commit(); }
+        catch (Exception e) {
+            //log.warning(e.getMessage());
+        }
+        finally {
+            if (txn.isActive()) txn.rollback();
+            em.close(); }
     }
 }
