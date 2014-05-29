e7ae6acf92f8eebcc67a30239e6e3e6131cbc493
Carlos de Dios <cdedios92@gmail.com>
2014-05-29 15:41:54 +0200

    EmailServlet added

--- src/cat/udl/eps/softarch/webglossary/model/User.java
+++ src/cat/udl/eps/softarch/webglossary/model/User.java
@@ -18,6 +18,8 @@ public class User {
     private String username;
     private String password;
 
+    public User(){};
+
     public User(String username, String password){
         this.username = username;
         this.password = password;
@@ -41,5 +43,15 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object other)
+    {
+        if (other == null) return false;
+        if (other == this) return true;
+        if (!(other instanceof Alert))return false;
+        User otherMyClass = (User) other;
+        return otherMyClass.getUsername() == this.username;
     }
 }
