e43481654544e5e8456626535790220833bf1164
Carlos de Dios <cdedios92@gmail.com>
2014-06-01 12:02:01 +0200

    ItineraryTest changed to see why it's not working

--- test/src/cat/udl/eps/softarch/webglossary/model/ItineraryTest.java
+++ test/src/cat/udl/eps/softarch/webglossary/model/ItineraryTest.java
@@ -1,6 +1,9 @@
 package cat.udl.eps.softarch.webglossary.model;
 
+import cat.udl.eps.softarch.webglossary.servlets.ItinerariesServlet;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 /**
  * Created by carlos on 5/29/14.
@@ -8,6 +11,11 @@ import org.junit.Test;
 public class ItineraryTest {
     @Test
     public void testAddItinerary() throws Exception {
-
+        double x=3.0;
+        double y= 67.5;
+        Itinerary it = new Itinerary("carlos@largos.com","C-24",x,y,true);
+        Itinerary.addItinerary(it);
+        ArrayList<Itinerary> ites = Itinerary.getStoredItineraries("carlos@largos.com");
+        System.out.println(ites.isEmpty());
     }
 }
